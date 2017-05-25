e2431734b51739110cd75414c49a66a25f375517
Pablo <pablo.gaete08@gmail.com>
2017-05-25 23:45:31 +0000

    front end bellezaestilo

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Kore\PageBundle\KorePageBundle(),
+            new Kore\FrontBundle\KoreFrontBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
