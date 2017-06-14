d1c6294d27095772207941d2132ebf8632556c6c
Garri <gfiguero@gmail.com>
2017-06-14 02:02:12 +0000

    agregadas entidades

--- src/Kore/FrontBundle/Controller/FrontController.php
+++ src/Kore/FrontBundle/Controller/FrontController.php
@@ -3,12 +3,30 @@
 namespace Kore\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Kore\AdminBundle\Entity\Photography;
+use Kore\AdminBundle\Entity\Header;
+use Kore\AdminBundle\Entity\Contact;
+use Kore\AdminBundle\Entity\Brand;
+use Kore\AdminBundle\Entity\Feature;
 
 class FrontController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('KoreFrontBundle:Front:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+//        $header = $em->getRepository('KoreAdminBundle:Header')->findOneById(1);
+        $photographies = $em->getRepository('KoreAdminBundle:Photography')->findAll();
+        $contacts = $em->getRepository('KoreAdminBundle:Contact')->findAll();
+        $brands = $em->getRepository('KoreAdminBundle:Brand')->findAll();
+        $features = $em->getRepository('KoreAdminBundle:Feature')->findAll();
+        return $this->render('KoreFrontBundle:Front:index.html.twig', array(
+            'photographies' => $photographies,
+            'contacts'      => $contacts,
+            'brands'        => $brands,
+            'features'      => $features,
+        ));
     }
     
     public function aboutAction()
--- web/js/bellezaestilo.js
+++ web/js/bellezaestilo.js
@@ -3,8 +3,6 @@
 (function($) {
     "use strict"; // Start of use strict
 
-<<<<<<< HEAD
-=======
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -14,15 +12,12 @@
         event.preventDefault();
     });
 
->>>>>>> 618b321da7762705690e6573e6c155e5d6e0feae
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
-        target: '#mainNav',
+        target: '.navbar-fixed-top',
         offset: 51
     });
 
-<<<<<<< HEAD
-=======
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){ 
             $('.navbar-toggle:visible').click();
@@ -35,5 +30,4 @@
         }
     })
 
->>>>>>> 618b321da7762705690e6573e6c155e5d6e0feae
 })(jQuery); // End of use strict
